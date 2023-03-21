78893b9e541923b66586abb31528ab551cd54b31
Jacob Keil <jacobkeil13@gmail.com>
2023-03-20 23:49:27 -0400

    init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav class="bg-[#384B52] h-12 text-white flex justify-between items-center">
+	<div class="flex justify-between items-center w-screen max-w-4xl mx-auto px-6">
+		<h1 class="font-bold tracking-wider max-w-lg">UNIVERSITY OF SOUTH FLORIDA</h1>
+		<div class="space-x-3 text-lg">
+			<a class="border-gray-500 border-r-2 px-3" href="http://">MyUSF</a>
+			<a class="" href="http://">Directory</a>
+		</div>
+	</div>
+</nav>
+<div class="bg-[#3F565E] h-[120px] flex justify-between items-center">
+	<div class="flex items-center w-screen max-w-4xl mx-auto px-6">
+		<h1 class="font-semibold tracking-wide text-4xl text-white">OFFICE OF FINANCIAL AID</h1>
+	</div>
+</div>
---
+++ src/store.js
@@ -0,0 +1,60 @@
+import { writable } from 'svelte/store';
+
+export const cc_data = writable({
+	student_information: {
+		level: '',
+		tuition: '',
+		semester: ''
+	},
+	tuition_fees: {
+		credit_hours: '',
+		lab_fees: '',
+		other_fees: ''
+	},
+	housing_food: {
+		living: '',
+		housing: '',
+		llc: '',
+		food_plan: ''
+	},
+	books_supplies: {
+		books: '',
+		supplies: ''
+	},
+	transportation: {
+		has_car: '',
+		other_transport: ''
+	},
+	personal_expenses: {
+		takeout_coffee: '',
+		groceries: '',
+		health_care: '',
+		personal_care: '',
+		phone_bill: '',
+		entertainment_social: '',
+		travel_trips: '',
+		subscriptions_memberships: '',
+		clothing: '',
+		family_expenses: '',
+		org_dues: '',
+		hobbies: '',
+		pets: '',
+		holidays_gifts: '',
+		laundry: ''
+	},
+	funding: {
+		grants: '',
+		scholarships: '',
+		loans: '',
+		other: '',
+		job_1: {
+			hourly_wage: '',
+			hours_per_week: ''
+		},
+		job_1: {
+			hourly_wage: '',
+			hours_per_week: ''
+		},
+		other_job: ''
+	}
+});
